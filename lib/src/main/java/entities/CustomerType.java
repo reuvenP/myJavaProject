63ccfbbbee5110e07626bb4ed4db2c4202edcc64
reuven.plevinsky <reuvenp@checkpoint.com>
2016-04-10 20:25:05 +0300

    delete lib

--- lib/src/main/java/entities/CustomerType.java
+++
@@ -1,8 +0,0 @@
-package entities;
-
-/**
- * Created by shmuel on 13/03/2016.
- */
-public enum CustomerType {
-    REGULAR, VIP
-}
--- lib/src/main/java/entities/Rateing.java
+++
@@ -1,8 +0,0 @@
-package entities;
-
-/**
- * Created by shmuel on 13/03/2016.
- */
-public enum Rateing {
-    ONE, TWO, THREE, FOUR, FIVE
-}
